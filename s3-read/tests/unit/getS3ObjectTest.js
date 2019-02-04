936ef1c01b7aa133f7d281422271f7b61a8d1448
Tatsuhiko Nakamine <nakamine@tenso.com>
2019-02-04 17:36:53 +0900

    :green_heart: add test code for getS3Object
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # ♻️  :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # for http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/

---
+++ s3-read/tests/unit/getS3ObjectTest.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const AWS = require('aws-sdk');
+const assert = require('power-assert');
+const getS3Object = require('../../services/getS3Object');
+
+const credentials = new AWS.SharedIniFileCredentials({profile: 'localstack'});
+AWS.config.credentials = credentials;
+
+const config = {
+  endpoint: 'http://localhost:4572',
+  s3ForcePathStyle: 'true',
+}
+const s3 = new AWS.S3(config);
+
+describe('local stack test suit for bucket object', () => {
+  let bucket, object, event;
+
+  /*
+   * create bucket and bucket object
+   */
+  before(async () => {
+    const now   = new Date().getTime();
+    bucket = `test-bucket-${now}`;
+    object = `message_${now}.txt`;
+    event = {
+      Bucket: bucket,
+      Key: object,
+    };
+    await s3.createBucket({Bucket: bucket}).promise();
+    await s3.putObject({
+      Bucket: bucket,
+      Key: object,
+      ContentType: 'text/plain',
+      Body: fs.readFileSync(path.join(__dirname, '..', 'fixtures', 'message.txt')),
+    }).promise();
+  });
+
+  it('successfully get an object', async () => {
+    const message = await getS3Object({s3, event, bucket});
+    assert.equal(message, 'Hi, there.\n');
+  });
+
+  // TODO: write test for non-exist bucket object
+
+  // TODO: write test for non-exist bucket
+
+  /*
+   * delete all bucket objects and remove bucket
+   */
+  after(async () => {
+    await s3.deleteObject({
+      Bucket: bucket,
+      Key: object,
+    }).promise();
+    await s3.deleteBucket({Bucket: bucket}).promise();
+  });
+});
